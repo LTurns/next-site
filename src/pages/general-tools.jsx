641d7218d154880518e40fc0f7046f220e6c1732
LTurns <lizzieturney@gmail.com>
2024-10-08 17:31:45 +0100

    finished products

---
+++ src/pages/general-tools.jsx
@@ -0,0 +1,36 @@
+import SEO from "@components/seo";
+import Wrapper from "@layout/wrapper";
+import Header from "@layout/header/header-01";
+import Footer from "@layout/footer/footer-01";
+import Breadcrumb from "@components/breadcrumb";
+import ExploreProductArea from "@containers/explore-product/layout-10";
+import productData from "../data/products-03.json";
+
+export async function getStaticProps() {
+    return { props: { className: "template-color-1" } };
+}
+
+const GeneralToolsProducts = productData.filter((product) =>
+    product.category.includes("General Tools")
+);
+
+const Product = () => (
+    <Wrapper>
+        <SEO pageTitle="General Tools Products" />
+        <Header />
+        <main id="main-content">
+            <Breadcrumb
+                pageTitle="General Tools Products"
+                currentPage="General Tools Products"
+            />
+            <ExploreProductArea
+                data={{
+                    products: GeneralToolsProducts,
+                }}
+            />
+        </main>
+        <Footer />
+    </Wrapper>
+);
+
+export default Product;
